e7236c2c67117ada251eddc5f6105a23d21b1739
joelvitorniino <joelprofessor13@gmail.com>
2022-04-05 19:35:29 -0300

    Main view controller

--- src/main/java/com/example/workshopjavafxjdbc/MainController.java
+++ src/main/java/com/example/workshopjavafxjdbc/MainController.java
@@ -1,5 +1,39 @@
 package com.example.workshopjavafxjdbc;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction() {
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction() {
+        System.out.println("onMenuItemDepartmentAction");
+    }
+
+    @FXML
+    public void onMenuItemAboutAction() {
+        System.out.println("onMenuItemAboutAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
