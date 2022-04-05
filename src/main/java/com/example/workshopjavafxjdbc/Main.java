926b098449d6abb7e126221ed9571e80a776b5e8
joelvitorniino <joelprofessor13@gmail.com>
2022-04-05 19:24:28 -0300

    correct

---
+++ src/main/java/com/example/workshopjavafxjdbc/Main.java
@@ -0,0 +1,34 @@
+package com.example.workshopjavafxjdbc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            ScrollPane scrollPane = loader.load();
+
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+
+            Scene mainScene = new Scene(scrollPane);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/example/workshopjavafxjdbc/MainController.java
@@ -0,0 +1,5 @@
+package com.example.workshopjavafxjdbc;
+
+public class MainController {
+
+}
