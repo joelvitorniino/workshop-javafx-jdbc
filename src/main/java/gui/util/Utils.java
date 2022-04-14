0d381f4dd747bc3e1a9bb0dbbcfa79e1409da289
joelvitorniino <joelprofessor13@gmail.com>
2022-04-13 21:53:31 -0300

    Seller TableView

--- src/main/java/gui/util/Utils.java
+++ src/main/java/gui/util/Utils.java
@@ -2,7 +2,12 @@ package gui.util;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class Utils {
     public static Stage currentStage(ActionEvent event) {
@@ -15,5 +20,46 @@ public class Utils {
         } catch(NumberFormatException e) {
             return null;
         }
+    }
+
+    public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Date> cell = new TableCell<T, Date>() {
+                private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+                @Override
+                protected void updateItem(Date item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    if(empty) {
+                        setText(null);
+                    } else {
+                        setText(sdf.format(item));
+                    }
+                }
+            };
+
+            return cell;
+        });
+    }
+
+    public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Double> cell = new TableCell<T, Double>() {
+
+                @Override
+                protected void updateItem(Double item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    if(empty) {
+                        setText(null);
+                    } else {
+                        setText(String.format("%." + decimalPlaces + "f", item));
+                    }
+                }
+            };
+
+            return cell;
+        });
     }
 }
